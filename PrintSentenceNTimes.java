e4c93f510a615b9580e71660bc9bfd6840b556dd
Hrishikesh yadav <103590267+Hrishi12072000@users.noreply.github.com>
2022-07-22 18:37:25 +0530

    Add files via upload

---
+++ PrintSentenceNTimes.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.util.Scanner;
+
+public class PrintSentenceNTimes 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		 int i,num;
+	     Scanner sc=new Scanner(System.in);
+	     System.out.println("Enter the number how many time you want to print sentence");
+	     num=sc.nextInt();
+	     for(i=1;i<=num;i++)
+	     {
+	    	 System.out.println("Hii,Hrishikesh is here");
+	     }
+	}
+}
