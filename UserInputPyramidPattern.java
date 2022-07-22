d7837117431ed0c793dadd6d77dbbd51e8def110
Hrishikesh yadav <103590267+Hrishi12072000@users.noreply.github.com>
2022-07-22 23:10:08 +0530

    Add files via upload

---
+++ UserInputPyramidPattern.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Scanner;
+
+public class UserInputPyramidPattern
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		{
+			// TODO Auto-generated method stub
+			int i,j;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the row for pyramid:");
+			int row=sc.nextInt();  //row = 3
+			
+			for(i=1; i<=row; i++) {
+				
+				//for loop for blank space
+				for(j=i;j<=row-1;j++)
+				{
+					System.out.print(" ");
+				}
+				//for loop for inner loop
+				for(j=1;j<=2*i-1;j++)  //OR j<2*i
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+	    }
+	}
+
+}
